dfae79ea71edb12e6d915f53da194764c90ae1a5
RaskinMaharjan <raskin0maharjan@gmail.com>
2020-10-04 21:30:49 +0545

    use htmlFor property in label tag

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './SearchBar.css';
 
 const searchBar = (props) => {
-console.log(props);
     return (
         <div className = 'searchBar'>
                 <input type ='text' placeholder='city name' maxLength='50' value= {props.city} onChange={props.handleCity}/>
+                   
                 <div className = 'unit'>
-                <input type ='radio' id = 'fahrenheit' name='units' value='imperial' checked= {props.unit === 'imperial'} onChange={props.handleUnit}/> 
-                <label for ='fahrenheit'>Fahrenheit</label>
-                <input type ='radio' id = 'celcius' name='units' value='metric' checked= {props.unit === 'metric'} onChange={props.handleUnit}/> 
-                <label for='celcius'>Celcius</label>
-                
-
+                        <input type ='radio' id = 'fahrenheit' name='units' value='imperial' checked= {props.unit === 'imperial'} onChange={props.handleUnit}/> 
+                        <label htmlFor ='fahrenheit'>Fahrenheit</label>
+                        
+                        <input type ='radio' id = 'celcius' name='units' value='metric' checked= {props.unit === 'metric'} onChange={props.handleUnit}/> 
+                        <label htmlFor='celcius'>Celcius</label>
                 </div>
+                
                 <button onClick={props.fetchData}> Get Forcast </button>
         </div>
     )
