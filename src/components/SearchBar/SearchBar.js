12ee301b2ad153410f002a3ae5248056aec62020
RaskinMaharjan <raskin0maharjan@gmail.com>
2020-10-04 21:20:34 +0545

    add basic css forcomponents

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import './SearchBar.css';
 
 const searchBar = (props) => {
 console.log(props);
     return (
         <div className = 'searchBar'>
                 <input type ='text' placeholder='city name' maxLength='50' value= {props.city} onChange={props.handleCity}/>
-                <input type ='radio' name='units' value='imperial' checked= {props.unit === 'imperial'} onChange={props.handleUnit}/> Fahrenheit
-                <input type ='radio' name='units' value='metric' checked= {props.unit === 'metric'} onChange={props.handleUnit}/> Celcius
+                <div className = 'unit'>
+                <input type ='radio' id = 'fahrenheit' name='units' value='imperial' checked= {props.unit === 'imperial'} onChange={props.handleUnit}/> 
+                <label for ='fahrenheit'>Fahrenheit</label>
+                <input type ='radio' id = 'celcius' name='units' value='metric' checked= {props.unit === 'metric'} onChange={props.handleUnit}/> 
+                <label for='celcius'>Celcius</label>
+                
+
+                </div>
                 <button onClick={props.fetchData}> Get Forcast </button>
         </div>
     )
